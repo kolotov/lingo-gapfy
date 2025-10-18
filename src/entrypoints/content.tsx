c10cae620050d29273c36f924b90a3b2f3363c23
gkdev <gkolotov.adv@gmail.com>
2025-10-18 14:04:01 +0300

    init commit

---
+++ src/entrypoints/content.tsx
@@ -0,0 +1,23 @@
+import ReactDOM from "react-dom/client";
+import {StartButton} from "@/components/StartButton/StartButton.tsx";
+
+export default defineContentScript({
+  matches: ['*://*.youtube.com/*'],
+  async main(ctx) {
+
+    const buttonUI = createIntegratedUi(ctx, {
+      position: "inline",
+      anchor: ".ytp-right-controls",
+      append: "first",
+      onMount: (container) => {
+        const root = ReactDOM.createRoot(container);
+        root.render(<StartButton/>);
+        return root;
+      },
+      onRemove: (root) => {
+        root?.unmount();
+      },
+    });
+    buttonUI.autoMount();
+  },
+});
